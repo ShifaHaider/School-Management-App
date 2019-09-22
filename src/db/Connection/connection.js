3c3d8f8b884de57c77751e35a7b1a1e02c7736bd
ShifaHaider <shifa.haider065@gmail.com.com>
2019-09-22 17:19:49 +0500

    updated

--- src/db/Connection/connection.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require("mongoose");
-var uri = "mongodb+srv://shifa:hello1@kutta-pgudg.gcp.mongodb.net/test?retryWrites=true";
-mongoose.connect(uri , {useNewUrlParser: true});
-
-var db = mongoose.connection;
